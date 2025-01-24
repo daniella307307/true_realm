90d62da070c314379b578d7773489ec9647c1f85
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2025-01-25 01:45:07 +0200

    [update]: dynamic app pages setting

---
+++ app/(izu-monitoring)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const IZUMonitoringScreen = () => {
+  return (
+    <View>
+      <Text>IZUMonitoringScreen</Text>
+    </View>
+  )
+}
+
+export default IZUMonitoringScreen
